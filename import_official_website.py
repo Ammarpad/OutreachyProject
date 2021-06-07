990997c8566de7fab4225af767af37f6a8565c9c
Ammarpad <ammarpad@yahoo.com>
2021-06-07 07:32:16 +0100

    add .gitignore

--- import_official_website.py
+++ import_official_website.py
@@ -6,7 +6,7 @@ OFFICIAL_WEBSITE_PROPERTY = 'P856'
 URL_REGEX = r'https?://[^\s<>"]+|www\.[^\s<>"]+'
 
 def do_import():
-	cat = 'Official_website_not_in_Wikidata'
+    cat = 'Official_website_not_in_Wikidata'
     en_wiki = pywikibot.Site('en', 'wikipedia')
 
     cat = pywikibot.Category(en_wiki, cat)
@@ -14,26 +14,26 @@ def do_import():
     pages = pagegenerators.CategorizedPageGenerator(cat, recurse=False)
     for page in pages:
         try:
-			data_item = pywikibot.ItemPage.fromPage(page)
-		except:
-			print("Skipping {}, no data item found", page.title())
-			no_data.append(page.title())
-			continue
+            data_item = pywikibot.ItemPage.fromPage(page)
+        except:
+            print("Skipping {}, no data item found", page.title())
+            no_data.append(page.title())
+            continue
 
-		website = extract_weblink(page)
+        website = extract_weblink(page)
 
-	if len(no_data):
-		import_script.record_pages_without_items(no_data, 'missing-data-items-list')
+    if len(no_data):
+        import_script.record_pages_without_items(no_data, 'missing-data-items-list')
 
 def extract_weblink(page):
-	page_source = page.expand_text(True)
-	html = BeautifulSoup(page_source)
-	link = html.findAll('span', {'class':'url', 'class':'official-website'})
+    page_source = page.expand_text(True)
+    html = BeautifulSoup(page_source)
+    link = html.findAll('span', {'class':'url', 'class':'official-website'})
 
-	if not items or len(items) != 1:
-		return None
+    if not items or len(items) != 1:
+        return None
 
-	return re.findall(URL_REGEX, link)[0]
+    return re.findall(URL_REGEX, link)[0]
 
 if __name__ == '__main__':
-	do_import()
+    do_import()
