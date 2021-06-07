66bb9840cc5f9f8f6685d509505b4da341a27cc4
Ammarpad <ammarpad@yahoo.com>
2021-06-07 08:01:05 +0100

    improve script

--- import_official_website.py
+++ import_official_website.py
@@ -11,23 +11,27 @@ URL_REGEX = r'https?://[^\s<>"]+|www\.[^\s<>"]+'
 def do_import():
     cat = 'Official_website_not_in_Wikidata'
     en_wiki = pywikibot.Site('en', 'wikipedia')
+    repo = en_wiki.data_repository()
+    catObj = pywikibot.Category(en_wiki, cat)
+    data = no_data_item = list()
 
-    cat = pywikibot.Category(en_wiki, cat)
-
-    pages = pagegenerators.CategorizedPageGenerator(cat, recurse=False)
+    pages = pagegenerators.CategorizedPageGenerator(catObj, recurse=False)
     for page in pages:
         try:
             data_item = pywikibot.ItemPage.fromPage(page)
         except:
             print("Skipping {}, no data item found", page.title())
-            no_data.append(page.title())
+            no_data_item.append(page.title())
             continue
 
         website = extract_weblink(page)
+        data.append(website, data_item)
         print("Found %s for %s:", website, page.title())
 
-    if len(no_data):
+    if len(no_data_item):
         import_script.record_pages_without_items(no_data, 'missing-data-items-list')
+
+    result = import_script.add_claims_to_item(repo, data, OFFICIAL_WEBSITE_PROPERTY, summary='')
 
 def extract_weblink(page):
     page_source = page.expand_text(True)
