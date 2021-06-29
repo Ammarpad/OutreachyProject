7674ce33e7cf5ade8a95601d1061de0c66d455c9
Ammarpad <ammarpad@yahoo.com>
2021-06-29 14:50:37 +0100

    update_gnd_id_qualifiers.py: Do not update target value

--- extra/update_gnd_id_qualifiers.py
+++ extra/update_gnd_id_qualifiers.py
@@ -18,8 +18,6 @@ STATED_AS = 'P1932'
 RETRIEVED = 'P813'
 SITE = pywikibot.Site('wikidata', 'wikidata')
 REPO = SITE.data_repository()
-SESSION = requests.Session()
-entityfacts = 'https://hub.culturegraph.org/entityfacts/'
 summary = '([[Wikidata:Requests for permissions/Bot/AmmarpadBot|update GND ID qualifier]])'
 
 def main(limit):
@@ -55,72 +53,10 @@ def main(limit):
         print('Finished. %s items updated.' %count)
     
 def updateQualifier(claim, qual):
-    val = getTargetVal(claim.target)
-    retrieved = True 
-
-    if not val:
-        val = qual.target
-        retrieved = False
-
     qualifier = pywikibot.Claim(REPO, NAMED_AS)
-    qualifier.setTarget(val)
+    qualifier.setTarget(qual.target)
     qualifier.hash = qual.hash
     claim.addQualifier(qualifier, summary=summary)
-    target_changed = qual.target != qualifier.target
-
-    if retrieved and target_changed:
-        retrieved = pywikibot.Claim(REPO, RETRIEVED)
-        now = datetime.now()
-        date = dict(year=now.year,
-                    month=now.month,
-                    day=now.day)
-        retrieved.setTarget(pywikibot.WbTime(**date))
-        claim.addSource(retrieved)
-
-    return target_changed
-
-def getTargetVal(id):
-    val = None
-    result = SESSION.get(entityfacts + id)
-    if result.status_code == 200:
-        res = result.json()
-        if res['@type'] == 'person':
-            preferredName = res.get('preferredName', None)
-
-            if preferredName:
-                return preferredName
-
-            pref = res.get('prefix', '')
-            sname = res.get('surname', '')
-            fname = res.get('forename', '')
-            if not fname or (fname and sname == ''):
-                return fname if fname else None
-
-            val = '%s %s' %(sname, fname)
-            val = ('%s %s' %(val, pref)) if pref else val
-
-    return val
-
-def getPreferredName(id):
-    """Alternative to getTargetVal(). This retrieves the
-    preferredNameForThePerson property if it exists"""
-    url = f'https://d-nb.info/gnd/{id}/about/lds'
-    key = 'https://d-nb.info/standards/elementset/gnd#preferredNameForThePerson'
-    result = SESSION.get(url)
-
-    if result.status_code != 200:
-        return None
-
-    data = Graph().parse(data=result.text, format='n3')
-    sdata = data.serialize(format='json-ld', indent=4)
-
-    try:
-        j = json.loads(sdata.decode())
-        name = j[0][key][0].get('@value')
-        return name if name else None
-    except:
-        return None
-
 
 if __name__ == '__main__':
     limit = int(sys.argv[1])
