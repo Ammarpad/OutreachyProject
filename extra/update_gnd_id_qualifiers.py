2597d5ac7dd7d2ec006d3dacc99b155f81669d6c
Ammarpad <ammarpad@yahoo.com>
2021-06-29 14:59:58 +0100

    simplify script

--- extra/update_gnd_id_qualifiers.py
+++ extra/update_gnd_id_qualifiers.py
@@ -8,7 +8,6 @@ from requests.exceptions import ConnectionError, ReadTimeout
 GND_ID = 'P227'
 NAMED_AS = 'P1810'
 STATED_AS = 'P1932'
-RETRIEVED = 'P813'
 SITE = pywikibot.Site('wikidata', 'wikidata')
 REPO = SITE.data_repository()
 summary = '([[Wikidata:Requests for permissions/Bot/AmmarBot|update GND ID qualifier]])'
@@ -25,20 +24,11 @@ def main(limit):
     retries = 5
     try:
         for item in items:
-            item.get()
             for claim in item.claims[GND_ID]:
                 if STATED_AS in claim.qualifiers:
                     for qual in claim.qualifiers[STATED_AS]:
-                        try:
-                            updateQualifier(claim, qual)
-                        except (ConnectionError, ReadTimeout):
-                            retries -= 1
-                            if retries > 0:
-                                pywikibot.sleep(5)
-                                updateQualifier(claim, qual)
-                            else:
-                                print('Connection problem. Quitting')
-                                sys.exit()
+                        updateQualifier(claim, qual)
+
                         count += 1
                         if count == limit:
                             raise StopIteration
