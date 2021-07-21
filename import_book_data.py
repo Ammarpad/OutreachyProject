98cf5e38a2df43de2b8fdeb786632cb342f99957
Ammarpad <ammarpad@yahoo.com>
2021-07-21 14:13:56 +0100

    Refactor claimExists() to allow multiple operation at the same
    Rename the function to checkClaims() so that we can check multiple
    claims at the same time. It now returns list of string or bool
    instead of bool only.
    
    Strings list is the ids that we can add; True means there's no
    data item. Empty list means all the claims already exist

--- import_book_data.py
+++ import_book_data.py
@@ -9,6 +9,7 @@ OCLC_1D = 'P243'
 PAGE_NUM_ID = 'P1104'
 BOOK_TEMPLATE = 'Infobox book'
 ISBN_ID = { 10: ISBN_10, 13: ISBN_13 }
+ALL_PROPS = (PAGE_NUM_ID, OCLC_1D, ISBN_13, ISBN_10)
 # For basic validation of structure for both ISBN- 10 and 13
 RE_ISBN = re.compile(r'^(97(8|9))?\d{9}(\d|X)$', re.I)
 
@@ -68,16 +69,21 @@ def getOCLC(templates):
 
     return None
 
-def claimExists(claimID, page):
-    """Checks the repo to find if
-    the claim already exists"""
+def checkClaims(claimIDs, page):
+    """Checks the repo to find if any of or all
+    of these claims already exist on the item"""
     try:
         item = page.data_item()
     except pywikibot.exceptions.NoPage:
-        # Pretend it does if don't even have data item
+        # Pretend it does if we don't even have data item
         return True
 
-    return claimID in item.get()['claims']
+    res = list()
+    for claimID in claimIDs:
+        if claimID not in item.get()['claims']:
+            res.append(claimID)
+
+    return res
 
 def getData(pages, limit):
     data = list()
@@ -94,16 +100,19 @@ def getData(pages, limit):
 
             if title in titles:
                 continue
-            elif claimExists(PAGE_NUM_ID, page):
-                file.write(title+'\n')
+
+            ids = checkClaims(ALL_PROPS):
+            if ids == True or ids == []:
                 continue
-            else:
-                temps = page.raw_extracted_templates
-                page_num = getPageNum(temps)
-                file.write(title+'\n')
-                if page_num:
-                    data.append([page_num, page.data_item()])
-                    count += 1
+
+            file.write(title+'\n')
+    
+            temps = page.raw_extracted_templates
+            page_num = getPageNum(temps)
+            file.write(title+'\n')
+            if page_num:
+                data.append([page_num, page.data_item()])
+                count += 1
 
             if limit == count:
                 break
