e722343ab93c8520828d1fd4d1f836bb925eb122
Ammarpad <ammarpad@yahoo.com>
2021-08-09 00:59:36 +0100

    Start new module: nft_data.py

---
+++ nft_data.py
@@ -0,0 +1,19 @@
+#!/usr/local/bin/python3
+
+import re
+import sys
+import common
+import pywikibot
+from pywikibot import pagegenerators
+
+
+WFN_PROP_ID = 'P2574'
+
+
+def  main():
+	pass
+
+
+
+if __name__ == '__main__':
+	main()
